608ea61d6047e8911a9878dc71c212accd54d1fc
Trismegiste <florent.g@allopneus.com>
2012-06-20 20:44:10 +0200

    factorial is working

--- solver.php
+++ solver.php
@@ -1020,13 +1020,13 @@ var(X) :- bagof(l, varTest(X), [l, l]).
 varTest(a).
 tarTest(b).
 
-factorial(1, 0).
+factorial(0, 1).
 factorial(N, X) :- compare(N, 0, gt), sub(N, 1, N1), factorial(N1, P), mul(N, P, X).
 
 
 BOZ;
 $query = "bagof(c, triple(sc, A, B), L), length(L, N) # L should have 21 elements";
-$query = "factorial(6)";
+$query = "factorial(6, X) # should return 720";
 
 execute($rules, $query)
 ?>
