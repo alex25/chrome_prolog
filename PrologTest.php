beb5f6f68bac2d45cfd7a369bb69c118f0047f23
Trismegiste <florent.g@allopneus.com>
2012-06-23 16:40:01 +0200

    Stub for PhpUnit

---
+++ PrologTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PrologTest
+ *
+ * @author flo
+ */
+class PrologTest extends PHPUnit_Framework_TestCase {
+
+    //put your code here
+    public function testBagOf() {
+        
+    }
+
+    public function testFactorial() {
+        
+    }
+
+    public function testQsort() {
+        
+    }
+
+    public function testCompare() {
+        
+    }
+
+}
+
+?>
