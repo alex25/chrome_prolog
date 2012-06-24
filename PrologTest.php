d826e14aeba9efd1f9fdd3872e1c6a88b13808ba
Trismegiste <florent.g@allopneus.com>
2012-06-24 22:16:18 +0200

    much beautiful testing

--- PrologTest.php
+++ PrologTest.php
@@ -9,26 +9,32 @@ require_once(__DIR__ . '/solver.php');
  */
 class PrologTest extends PHPUnit_Framework_TestCase {
 
+    protected function setUp() {
+        $this->program = file_get_contents('fixtures_test.pro');
+    }
+
+    protected function tearDown() {
+        unset($this->program);
+    }
+
     public function queryProvider() {
-        $fixtures = file_get_contents('fixtures_test.pro');
         return array(
-            array($fixtures, 'bagof(c, triple(sc, A, B), L), length(L, N)', 'N', 21),
-            array($fixtures, 'factorial(6, X)', 'X', 720),
-            array($fixtures, 'qsort([5,3,2,111,88,9,7], X)', 'X', '[2, 3, 5, 7, 9, 88, 111]')
+            array('bagof(c, triple(sc, A, B), L), length(L, N)', 'N', 21),
+            array('factorial(6, X)', 'X', 720),
+            array('qsort([5,3,2,111,88,9,7], X)', 'X', '[2, 3, 5, 7, 9, 88, 111]')
         );
     }
 
     /**
      * @dataProvider queryProvider
      */
-    public function testQuery($db, $query, $name, $val) {
-        $result = execute($db, $query);
+    public function testQuery($query, $name, $val) {
+        $result = execute($this->program, $query);
         $this->assertEquals($val, $result->$name);
     }
-    
+
     public function testLessOrEqual() {
-        $fixtures = file_get_contents('fixtures_test.pro');
-        $result = execute($fixtures, 'leq(33, 7)');
+        $result = execute($this->program, 'leq(33, 7)');
         $this->assertTrue($result->isSuccess());
     }
 
